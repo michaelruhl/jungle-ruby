6970bf6a9d7ec4e79f079baf3f3026c36c56d311
Michael Ruhl <michaelruhl14@hotmail.com>
2022-08-30 18:04:36 -0400

    cypress products page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,17 @@
+// const { first } = require("cypress/types/lodash");
+
+describe('example to-do app', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000')
+      
+    })
+    it("There is products on the page", () => {
+        cy.get(".products article").should("be.visible");
+      });
+      it("There is 2 products on the page", () => {
+        cy.get(".products article").should("have.length", 2);
+      });
+      it("Click first product on page", () => {
+        cy.get('.products article').first().click()
+      });
+})
